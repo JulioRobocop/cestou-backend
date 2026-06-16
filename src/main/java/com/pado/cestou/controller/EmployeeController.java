9d170d187e298dc6b2902a57d07a5ebeddda6469
JulioRobocop <juliocesar.gp44@gmail.com>
2026-06-16 11:16:47 -0300

    feat: Add two news endpoints ('/me','/logout') and modified existing ones to get the cookies

--- src/main/java/com/pado/cestou/controller/EmployeeController.java
+++ src/main/java/com/pado/cestou/controller/EmployeeController.java
@@ -2,9 +2,13 @@ package com.pado.cestou.controller;
 
 import com.pado.cestou.model.Employee;
 import com.pado.cestou.service.EmployeeService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/employees")
@@ -15,20 +19,60 @@ public class EmployeeController {
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
-    
+
+    @GetMapping("/me")
+    public Employee getCurrentUser(@AuthenticationPrincipal Employee employee) {
+        return employee;
+    }
 
     @PostMapping("/register")
-    public Map<String, String> registerUser(@RequestBody Employee employee) {
+    public Map<String, String> registerUser(@RequestBody Employee employee, HttpServletResponse response) {
         String token = employeeService.register(employee);
-        return Map.of("token", token);
+        ResponseCookie cookie = ResponseCookie.from("token", token)
+                .httpOnly(true)
+                .secure(false) // Change to true when in production (with HTTPS)
+                .path("/")
+                .maxAge(60 * 60 * 24)
+                .sameSite("Lax")
+                .build();
+        response.addHeader("Set-cookie", cookie.toString());
+        return Map.of("message", "registro realizado com sucesso",
+                "name", employee.getName(),
+                "id", employee.getId().toString()
+        );
     }
 
 
     @PostMapping("/login")
-    public Map<String, String> loginUser(@RequestBody Map<String, String> input) {
+    public Map<String, String> loginUser(@RequestBody Map<String, String> input, HttpServletResponse response) {
         int registration = Integer.parseInt(input.get("registration"));
         String password = input.get("password");
         String token = employeeService.login(registration, password);
-        return Map.of("token", token);
+        ResponseCookie cookie = ResponseCookie.from("token", token)
+                .httpOnly(true)
+                .secure(false) // Change to true when in production (with HTTPS)
+                .path("/")
+                .maxAge(60 * 60 * 24) // 24 hours
+                .sameSite("Lax")
+                .build();
+        response.addHeader("Set-cookie", cookie.toString());
+        Employee found = employeeService.findByRegistration(registration).get();
+        return Map.of("message", "Login realizado com sucesso",
+                "name", found.getName(),
+                "id", found.getId().toString()
+        );
+    }
+
+    @PostMapping("/logout")
+    public Map<String, String> logout(HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from("token", "")
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Lax")
+                .build();
+        response.addHeader("Set-Cookie", cookie.toString());
+        return Map.of("message", "Logout realizado com sucesso");
     }
 }
