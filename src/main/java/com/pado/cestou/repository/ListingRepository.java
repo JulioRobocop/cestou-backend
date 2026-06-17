a3da0cb6f67943a6c613bf9ce527e119a5de8bd5
JulioRobocop <juliocesar.gp44@gmail.com>
2026-06-17 16:10:52 -0300

    feat: Add Query for workshift and sector filters

--- src/main/java/com/pado/cestou/repository/ListingRepository.java
+++ src/main/java/com/pado/cestou/repository/ListingRepository.java
@@ -1,15 +1,23 @@
 package com.pado.cestou.repository;
 
-import com.pado.cestou.model.Employee;
-import com.pado.cestou.model.Listing;
-import com.pado.cestou.model.Status;
+import com.pado.cestou.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface ListingRepository extends JpaRepository<Listing, Long> {
-    List<Listing> findByStatus(Status status);
     List<Listing> findBySeller(Employee seller);
     List<Listing> findByBuyer(Employee buyer);
+    @Query("SELECT l FROM Listing l WHERE l.status = :status " +
+            "AND (:sector IS NULL OR l.seller.sector = :sector) " +
+            "AND (:workShift IS NULL OR l.seller.workShift = :workShift)")
+    List<Listing> findAvailableWithFilters(
+            @Param("status") Status status,
+            @Param("sector") Sector sector,
+            @Param("workShift")WorkShift workShift
+            );
 }
 
 
